3948603997cf0a3c4940310a70b6ac0d831cdabc
cborja36 <cborja36@gmail.com>
2022-11-03 12:34:31 +0100

    Basic autocompletion

--- lua/borja/plugins-setup.lua
+++ lua/borja/plugins-setup.lua
@@ -55,6 +55,16 @@ return packer.startup(function(use)
 	-- statusline
 	use("nvim-lualine/lualine.nvim")
   
+  -- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
+
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
