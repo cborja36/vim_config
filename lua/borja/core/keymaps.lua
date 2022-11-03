b4931a6e2aa80d4c0d6a85f5a6ffb838844b69be
cborja36 <cborja36@gmail.com>
2022-11-03 13:50:03 +0100

    New keybinding for creating blank line in normal mode

--- lua/borja/core/keymaps.lua
+++ lua/borja/core/keymaps.lua
@@ -31,6 +31,10 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+-- intert empty line without entering insert mode
+keymap.set("n", "<leader>o", "o<Esc>")
+keymap.set("n", "<leader>O", "O<Esc>")
+
 ----------------------
 -- Plugin Keybinds
 ----------------------
